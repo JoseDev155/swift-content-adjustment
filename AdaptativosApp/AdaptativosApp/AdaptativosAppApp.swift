f8aaccfa1ee169007a519cd38509892a389ef277
Jose <jeduramos7001@gmail.com>
2026-05-11 00:00:34 -0700

    Add AdaptativosApp project base

---
+++ AdaptativosApp/AdaptativosApp/AdaptativosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdaptativosAppApp.swift
+//  AdaptativosApp
+//
+//  Created by Jose Ramos on 10/5/26.
+//
+
+import SwiftUI
+
+@main
+struct AdaptativosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
