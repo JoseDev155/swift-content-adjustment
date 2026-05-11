ac41b056bf10840288d0fc892a8cceb01017751f
Jose <jeduramos7001@gmail.com>
2026-05-10 19:55:18 -0700

    Add SpacerrApp project base

---
+++ SpacerApp/SpacerApp/SpacerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpacerAppApp.swift
+//  SpacerApp
+//
+//  Created by Jose Ramos on 10/5/26.
+//
+
+import SwiftUI
+
+@main
+struct SpacerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
